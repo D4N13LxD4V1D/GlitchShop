ed6edb71038f56b43fe1af28f930da99c0822833
Marilag <offcllance@gmail.com>
2022-12-07 16:56:41 +0800

    Create test.php
    Temporary separation of front-end and back-end for easier editing and later merging.

---
+++ test.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang = "en">
+    <head>
+        <meta charset = "UTF-8">
+        <meta http-equiv = "X-UA-Compatible" content = "IE=edge">
+        <meta name = "viewport" content = "width = device-width, initial-scale = 1.0">
+        <title> Glitch </title>
+
+        <link rel = "stylesheet" type = "text/css" href = "//db.onlinewebfonts.com/c/6dbf0126ffd861046c0120a7bbf66356?family=Utonium"/>
+        <link rel = "stylesheet" href = "mycss.css" />
+        <script src = "myjs.js"></script>
+    </head>
+    <body>
+        <div class = "top">
+            <video id = "promo" height = "100%" width = "100%" autoplay muted loop >
+                <source src = "media/GlitchMode.mp4" type = "video/mp4">
+            </video>
+        </div>
+        <div class = "body">
+            <div class = "navbar">
+                <div id = "links">
+                    <a href = "/"><img height = "50px" src = "media/GlitchMode.png" /></a>
+                    <a href = "checkout.php"> ABOUT </a>
+                    <a href = "checkout.php"> SERVICES </a>
+                    <a href = "checkout.php"> CONTACT INFORMATION </a>
+                </div>
+                <div id = "icons">
+                    <a href = "/"><img height = "30px" src = "media/checkout.png" /></a>
+                    <a href = "/"><img height = "30px" src = "media/user.png" /></a>
+                    <div id = "nametag"> PLAYER ACCOUNT: <span id = "name">Fragrant</span></div>
+                </div>
+            </div>
+            <div class = "shop">
+                This is the shop.
+            </div>
+        </div>
+    </body>
+</html>
